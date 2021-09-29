0d47fe9e1238c2e06b6fb409693693bec90974b3
jackson.dalexander <jackson.dalexander@MU-C1-10.mullen.pvt.k12.co.us>
2021-09-29 12:10:50 -0600

    Added Changes

--- NFL Project/src/Teams.java
+++ NFL Project/src/Teams.java
@@ -1,4 +1,40 @@
-
+import java.util.ArrayList;
 public class Teams {
-
+	static ArrayList<Stats> NFLTeams = new ArrayList<Stats>();
+	
+	public static void fillTeams() {
+	NFLTeams.add(new Stats("Arizona Cardinals", 3, 0, 35, 50));
+	NFLTeams.add(new Stats("Atlanta Falcons", 1, 2, 35, 50));
+	NFLTeams.add(new Stats("Baltimore Ravens", 2, 1, 35, 50));
+	NFLTeams.add(new Stats("Buffalo Bills", 2, 1, 35, 50));
+	NFLTeams.add(new Stats("Carolina Panthers", 3, 0, 35, 50));
+	NFLTeams.add(new Stats("Chicago Bears", 1, 2, 35, 50));
+	NFLTeams.add(new Stats("Cincinnati Bengals", 2, 1, 35, 50));
+	NFLTeams.add(new Stats("Cleveland Browns", 2, 1, 35, 50));
+	NFLTeams.add(new Stats("Dallas Cowboys", 1, 2, 35, 50));
+	NFLTeams.add(new Stats("Denver Broncos", 1, 2, 35, 50));
+	NFLTeams.add(new Stats("Detroit Lions", 1, 2, 35, 50));
+	NFLTeams.add(new Stats("Green Bay Packers", 1, 2, 35, 50));
+	NFLTeams.add(new Stats("Houston Texans", 1, 2, 35, 50));
+	NFLTeams.add(new Stats("Indianapolis Colts", 1, 2, 35, 50));
+	NFLTeams.add(new Stats("Jacksonville Jaguars", 1, 2, 35, 50));
+	NFLTeams.add(new Stats("Kansas City Chiefs", 1, 2, 35, 50));
+	NFLTeams.add(new Stats("Las Vegas Raiders", 1, 2, 35, 50));
+	NFLTeams.add(new Stats("Los Angeles Chargers", 1, 2, 35, 50));
+	NFLTeams.add(new Stats("Los Angeles Rams", 1, 2, 35, 50));
+	NFLTeams.add(new Stats("Miami Dolphins", 1, 2, 35, 50));
+	NFLTeams.add(new Stats("Minnesota Vikings", 1, 2, 35, 50));
+	NFLTeams.add(new Stats("New England Patriots", 1, 2, 35, 50));
+	NFLTeams.add(new Stats("New Orleans Saints", 1, 2, 35, 50));
+	NFLTeams.add(new Stats("New York Giants", 1, 2, 35, 50));
+	NFLTeams.add(new Stats("New York Jets", 1, 2, 35, 50));
+	NFLTeams.add(new Stats("Philadelphia Eagles", 1, 2, 35, 50));
+	NFLTeams.add(new Stats("Pittsburgh Steelers", 1, 2, 35, 50));
+	NFLTeams.add(new Stats("San Francisco 49ers", 1, 2, 35, 50));
+	NFLTeams.add(new Stats("Seattle Seahawks", 1, 2, 35, 50));
+	NFLTeams.add(new Stats("Tampa Bay Buccaneers", 1, 2, 35, 50));
+	NFLTeams.add(new Stats("Tennessee Titans", 1, 2, 35, 50));
+	NFLTeams.add(new Stats("Washington Football Team", 1, 2, 35, 50));
+	
+	}
 }
