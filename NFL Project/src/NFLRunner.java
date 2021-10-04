d3cc37fbbcc60aef6ab626a8ca852b715e69d8db
jackson.dalexander <jackson.dalexander@MU-C1-10.mullen.pvt.k12.co.us>
2021-10-04 11:41:25 -0600

    added chages

--- NFL Project/src/NFLRunner.java
+++ NFL Project/src/NFLRunner.java
@@ -3,13 +3,31 @@ import java.util.Scanner;
 
 public class NFLRunner {
 	
+	
 	public static void main(String[] args) {
+		Teams.fillTeams();
 		Scanner userInput = new Scanner(System.in);
 		System.out.println("This is a program that will ask for the user to plug in new stats for an NFL team, and it will add them to the current stats that the team has. What NFL team would you like to research?");
-		//String team = userInput.nextLine();
+		for(int i = 0; i < Teams.NFLTeams.size(); i++) {
+			System.out.print((i + 1) + ": ");
+			System.out.println(Teams.NFLTeams.get(i).getTeamName());
+			System.out.println("Wins: " + Teams.NFLTeams.get(i).getWins());
+			System.out.println("Losses: " + Teams.NFLTeams.get(i).getLosses());
+			System.out.println("Total points scored: " + Teams.NFLTeams.get(i).getPointsScored());
+			System.out.println("Total points allowed: " + Teams.NFLTeams.get(i).getPointsAllowed());
+			System.out.println(" ");
+		}	
+		String team = userInput.nextLine();
+		switch(team){
+		case "Arizona Cardinals":
+			System.out.println(Teams.NFLTeams.get(0).getTeamName());
+			
+		default:
+			System.out.println("Hello");
+		}
 		
 		
-System.out.println("cool");
+		
 	}
 
 }
